86cb6868e0599caf5018e9f45676fc0591ede0a8
Evgeniy <eugeniymokrushin@gmail.com>
2022-08-31 19:28:15 +0300

    done +- (css needs modifications)

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Question from './components/Question'
 import { nanoid } from "nanoid"
 import './dist/css/index.css'
+import { set } from 'mongoose'
 
 
 
@@ -10,40 +11,79 @@ let questionsArray
 
 
 function App() {
-  const [start, setStart] = React.useState(false)
-  const [ansersSubmitted] = React.useState([])
+  const [start, setStart] = React.useState(true)
+  const [correctAns, setCorrectAns] = React.useState(0)
+  const [again, setAgain] = React.useState(true)
+  const [checkAnswers, setCheckAnswers] = React.useState(false)
   const [question, setQuestion] = React.useState()
 
   React.useEffect(() => {
+
     fetch(`https://opentdb.com/api.php?amount=${numberOfQuestions}`)
       .then((response) => response.json())
-      .then((data) => setQuestion(() => {
-        return (
-          {
-            ...data,
+      .then((data) => !start && setQuestion(() => {
+        let idData = []
+        data.results.forEach(element => {
+          let correct = element.correct_answer
+          const allAnswers = element.incorrect_answers
+          allAnswers.push(element.correct_answer)
+
+          let shuffledAnswers = allAnswers
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value)
+
+          idData.push({
+            ...element,
+            selected: '',
+            correct_answer: correct,
+            allAnswers: shuffledAnswers,
             id: nanoid()
-          }
-        )
-      }))
-  }, [start])
-  console.log(question)
-  function handleChange(id) {
-    console.log(id)
+          })
+        });
+
+        return idData
+      }));
+  }, [start, again])
+
+
+  function handleChange(event) {
+    const { name, value} = event.target
+    let newQuestions = question.map(element => {
+      return (element.id === name ? { ...element, selected: value, correct_answer: element.correct_answer } : element)
+    })
+    setQuestion(newQuestions)
   }
 
   function startEndQuiz() {
     setStart(prevState => !prevState)
   }
 
+  function checkAns() {
+    let numberCor = 0
+    question.forEach(q => {
+      q.correct_answer === q.selected ? numberCor += 1 : null
+    });
+    setCheckAnswers(prevState => !prevState)
+    setCorrectAns(numberCor)
+  }
+
+  function newGame() {
+    setAgain(prevState => !prevState)
+    setCheckAnswers(prevState => !prevState)
+    setQuestion(null)
+  }
+
   function getQuestions() {
     if (question != undefined) {
-      questionsArray = question.results.map(data => {
+      questionsArray = question.map(data => {
         return (
           <Question
-            key={data.id}
+            key={nanoid()}
             id={data.id}
-            data={data.results}
+            data={data}
             select={handleChange}
+            checking={checkAnswers}
           />
         )
       })
@@ -57,7 +97,7 @@ function App() {
       {start &&
         <div className="start">
           <h1 className='title'>Quizzical</h1>
-          <p className="description">Some description if needed</p>
+          <p className="description">Random questions about everything!</p>
           <button className="start-quiz" onClick={startEndQuiz}>Start quiz</button>
         </div>
       }
@@ -66,11 +106,11 @@ function App() {
           <div className="q-wrapper">
             {getQuestions()}
           </div>
-          <button className='check--answers'>Check answers</button>
-          {/* <div className="wrapper--again">
-            <div className="result">You scored 3/5 correct answers</div>
-            <button className="again">Play again</button>
-          </div> */}
+          {!checkAnswers && <button className='check--answers' onClick={checkAns}>Check answers</button>}
+          {checkAnswers && <div className="wrapper--again">
+            <div className="result">You scored {correctAns}/{numberOfQuestions} correct answers</div>
+            <button className="again" onClick={newGame}>Play again</button>
+          </div>}
         </main>
       }
     </div>
--- vite-project/src/components/Question.jsx
+++ vite-project/src/components/Question.jsx
@@ -1,30 +1,54 @@
 import React from "react";
 import { nanoid } from "nanoid"
 
-export default function ({id, data, select}) {
-    const answers = data.incorrect_answers
-    answers.push(data.correct_answer)
+export default function ({ id, data, select, checking }) {
 
+    
+    
     function decodeHtml(html) {
         var txt = document.createElement("textarea");
         txt.innerHTML = html;
         return txt.value;
     }
+    const ansersRender = data.allAnswers.map(answer => {
+        let id_status
+        if (checking) {
+            if (answer === data.correct_answer) {
+                id_status = 'correct'
+            } else if (answer === data.selected && answer !== data.correct_answer) {
+                id_status = "incorrect"
+            } else {
+                id_status = "notSelected"
+            }
+        }
 
-    const ansersRender = answers.map(answer => {
-        const id = nanoid()
+        const id_answer = nanoid()
         return (
-            <div className="answers" key={id} id={id}>
-                <label className="label--answer" htmlFor={id}>{decodeHtml(answer)}</label>
-                <input className="inp" type="radio" id={id} name={id} onChange={() => select(id)} />
+            <div key={id_answer} className="wrpapper-wrapper">
+                <label
+                    className={data.selected === answer ? "label--answer selected" : "label--answer"}
+                    id={id_status}
+                    htmlFor={id_answer}
+                >
+                    {decodeHtml(answer)}
+                </label>
+                <input
+                    id={id_answer}
+                    className="inp"
+                    type="radio"
+                    name={id}
+                    checked={data.selected === answer}
+                    onChange={(e) => select(e)}
+                    value={answer}
+                />
             </div>
         )
     })
 
     return (
-        <div className="question--block">
+        <div className="question--block" id={id}>
             <h2 className="question">{decodeHtml(data.question)}</h2>
-            <div className="wrapper--answers">
+            <div className="wrapper--answers" id={id}>
                 {ansersRender}
             </div>
         </div>
